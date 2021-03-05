cae7e3ef8067e103ea4f313484f4496532fc47fa
tran.hoang.loc <tran.hoang.loc@suns-Mac-mini-6.local>
2021-03-05 15:10:52 +0700

    fix config RNF admob

--- android/app/src/main/java/com/bloodycotech001/MainApplication.java
+++ android/app/src/main/java/com/bloodycotech001/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 // import com.airbnb.android.react.lottie.LottiePackage;
-import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
+import io.invertase.firebase.admob.ReactNativeFirebaseAdmobPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new LottiePackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new RNFirebaseAdMobPackage());
           return packages;
         }
 
